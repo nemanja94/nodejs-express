8fc73fe53593958270156e3a2bc806d598d13a2f
Nemanja Andric <aandric.nemanja@gmail.com>
2023-11-22 09:47:28 +0100

    Update and rename skylead_test.sql to test.sql
    Signed-off-by: Nemanja Andric <aandric.nemanja@gmail.com>

--- test.sql
+++ test.sql
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `skylead_test`
+-- Database: `test`
 --
-CREATE DATABASE IF NOT EXISTS `skylead_test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-USE `skylead_test`;
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `test`;
 
 -- --------------------------------------------------------
 
